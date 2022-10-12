693177baf95f6a03b1ad9f3a3e6d4c64e448cc76
Hitesh <hiteshankodia@gmail.com>
2022-10-12 13:20:51 +0530

    Initial commit

---
+++ shoppinglyx/app/models.py
@@ -0,0 +1,105 @@
+from email.policy import default
+from itertools import product
+from msilib.schema import Class
+from random import choice
+from sre_constants import CATEGORY
+from sre_parse import State
+from turtle import title
+from unicodedata import category
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator,MinValueValidator
+
+STATE_CHOICES = (
+    ('Andaman & Nicobar Islands', 'Andaman & Nicobar Islands'),
+    ('Andhra pradesh', 'Andhra pradesh'),
+    ('Arunachal Pradesh','Arunachal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chandigharh','Chandigharh'),
+    ('Chhattisgarh','Chhattisgarh'),
+    ('Dadra & Nagar Haveli','Dadra & Nagar Haveli'),
+    ('Daman and diu','Daman and diu'),
+    ('Delhi','Delhi'),
+    ('Goa', 'Goa'),
+    ('Gujrat','Gujrat'),
+    ('Haryana','Haryana'),
+    ('Himachal Pradesh','Himachal Pradesh'),
+    ('Jammu and kashmir','Jammu and kashmir'),
+    ('Jharkhand','Jharkhand'),
+    ('Karnataka','Karnataka'),
+    ('Kerela','Kerela'),
+    ('Lakshadweep','Lakshadweep'),
+    ('Madhya Pradesh','Madhya Pradesh'),
+    ('Maharastra','Maharastra'),
+    ('Manipur','Manipur'),
+    ('Meghalaya','Meghalaya'),
+    ('Mizoram','Mizoram'),
+    ('Nagaland','Nagaland'),
+    ('Odisha','Odisha'),
+    ('Punducherry','Punducherry'),
+    ('Punjab','Punjab'),
+    ('Rajasthan','Rajasthan'),
+    ('Sikkim','Sikkim'),
+    ('Tamil Nadu','Tamil Nadu'),
+    ('Telangana','Telangana'),
+    ('Uttarakhand','Uttarakhand'),
+    ('West Bengal','West Bengal'),
+)
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length = 50) 
+    zipcode = models.IntegerField() 
+    state = models.CharField(choices=STATE_CHOICES,max_length=50)
+    def __str__(self):
+        return str(self.id)
+
+CATEGORY_CHOICES = (
+    ('M','Mobile'),
+    ('L','Laptop'),
+    ('TW','Top Wear'),
+    ('BW', 'Bottom Wear'),    
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField() 
+    discounted_price = models.FloatField() 
+    description = models.TextField() 
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    product_image = models.ImageField(upload_to = 'productimg')
+    
+    def __str__(self):
+        return str(self.id)
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return str(self.id)
+
+
+STATUS_CHOICES = (
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel')
+)
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete= models.CASCADE) 
+    quantity = models.PositiveIntegerField(default = 1)
+    ordered_date = models.DateTimeField(auto_now_add = True)
+    status = models.CharField(max_length=50,
+    choices = STATUS_CHOICES,default='Pending') 
---
+++ shoppinglyx/app/views.py
@@ -0,0 +1,56 @@
+from itertools import product
+from msilib.schema import Class
+from unicodedata import category
+from django.shortcuts import render
+from django.views import View
+#from numpy import product
+from .models import Customer, Product, Cart, OrderPlaced
+#def home(request):
+ #return render(request, 'app/home.html')
+
+class ProductView(View):
+    def get(self, request):
+        topwears = Product.objects.filter(category= 'TW')
+        bottomwears = Product.objects.filter(category= 'BW')
+        mobiles = Product.objects.filter(category= 'M')
+        return render(request,'app/home.html',
+        {'topwears':topwears,'bottomwears':bottomwears,'mobiles':mobiles})
+
+#def product_detail(request):
+ #return render(request, 'app/productdetail.html')
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html',{'product':product})
+        
+
+def add_to_cart(request):
+ return render(request, 'app/addtocart.html')
+
+def buy_now(request):
+ return render(request, 'app/buynow.html')
+
+def profile(request):
+ return render(request, 'app/profile.html')
+
+def address(request):
+ return render(request, 'app/address.html')
+
+def orders(request):
+ return render(request, 'app/orders.html')
+
+def change_password(request):
+ return render(request, 'app/changepassword.html')
+
+def mobile(request):      
+    return render(request, 'app/mobile.html',)
+
+def login(request):
+ return render(request, 'app/login.html')
+
+def customerregistration(request):
+ return render(request, 'app/customerregistration.html')
+
+def checkout(request):
+ return render(request, 'app/checkout.html')
